fb479d1772a7bbee14cebcf53eb09cfdc2e976a3
Cristian <cristiansalgado312@gmail.com>
2023-04-26 20:35:38 -0500

    Vinculacion con Firebase

---
+++ firebase/cliente.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC1aT2pbbETj5I5gCbDbYq5Z-ISKbymBfY",
+    authDomain: "centroprogresa-bda09.firebaseapp.com",
+    projectId: "centroprogresa-bda09",
+    storageBucket: "centroprogresa-bda09.appspot.com",
+    messagingSenderId: "1034168296920",
+    appId: "1:1034168296920:web:a6dd177bb48874cc847ead",
+    measurementId: "G-1D8NJ1ZMYJ"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const loginWithGoogle = () => {
+    
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,64 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image"
+import logo from "../public/img/PractiYa_Blanco.png"
+import icono from "../public/img/facebook.png"
+import icono_2 from "../public/img/google.png"
+import {loginWithGoogle} from '../firebase/cliente'
 
 export default function Home() {
-  return (
-    <h1>Hola Mundo</h1>
-  )
+    return (
+        <>
+            <div class="container" id="container">
+                <div class="form-container sign-up-container">
+                    <form action="#">
+                        <h1>Registrate en PractiYa</h1>
+                        <div class="social-container">
+                            <a href="#" class="social">
+                                <Image src={icono.src} width={74} height={74} alt='Imagen Logotipo' />
+                            </a>
+                            <a href="#" class="social">
+                                <Image src={icono_2.src} width={74} height={74} alt='Imagen Logotipo' />
+                            </a>
+                        </div>
+                        <span>Ingresa con tu correo!</span>
+                        <input type="text" placeholder="Nombre"></input>
+                        <input type="email" placeholder="Correo" />
+                        <input type="password" placeholder="Contraseña" />
+                        <button className="btnRegistrar">Registrate</button>
+                    </form>
+                </div>
+                <div class="form-container sign-in-container">
+                    <form action="#">
+                        <h1>Ingresa a PractiYa</h1>
+                        <div class="social-container">
+                            <a href="#" class="social">
+                                <Image src={icono.src} width={74} height={74} alt='Imagen Logotipo' />
+                            </a>
+                            <a href="#" class="social">
+                                <Image src={icono_2.src} width={74} height={74} alt='Imagen Logotipo' />
+                            </a>
+                        </div>
+                        <span>Ingresa con tu correo!</span>
+                        <input type="email" placeholder="Correo" />
+                        <input type="password" placeholder="Contraseña" />
+                        <a href="#">¿Olvidaste tu contraseña?</a>
+                        <button className="btnIngresar">Ingresa</button>
+                    </form>
+                </div>
+                <div class="overlay-container">
+                    <div class="overlay">
+                        <div class="overlay-panel overlay-left">
+                            <h1>¡Hola, amigo!</h1>
+                            <Image src={logo.src} width={300} height={40} alt='Imagen Logotipo' />
+                            <button class="ghost" id="signIn">Inicia Sesion</button>
+                        </div>
+                        <div class="overlay-panel overlay-right">
+                            <h1>¡Bienvenid@!</h1>
+                            <Image src={logo.src} width={300} height={40} alt='Imagen Logotipo' />
+                            <button class="ghost" id="signUp">Registrate</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
